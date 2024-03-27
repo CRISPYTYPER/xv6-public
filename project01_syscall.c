6317874add5cc4b33c790d6a9b23faeaaea0f0d2
CRISPYTYPER <jack3409@naver.com>
2024-03-27 05:53:36 +0000

    add include statments since compile error occur

--- project01_syscall.c
+++ project01_syscall.c
@@ -1,6 +1,11 @@
 #include "types.h"
 #include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
 #include "proc.h"
+#include "x86.h"
+#include "syscall.h"
 
 // System call for returning parent PID
 int
