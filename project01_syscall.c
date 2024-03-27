fffe0a14c1a8f8cde4040ccedde08099a46d17f3
CRISPYTYPER <jack3409@naver.com>
2024-03-27 15:40:32 +0000

    set aside only the system calls

--- project01_syscall.c
+++ project01_syscall.c
@@ -7,9 +7,31 @@
 #include "x86.h"
 #include "syscall.h"
 
-// Kernel function for returning parent PID
+// // Kernel function for returning parent PID
+// int
+// getppid(void)
+// {
+//   struct proc *currentProcess = myproc();  // Fetch the current process structure.
+//   // Check if the current process or its parent is NULL.
+//   if(currentProcess == 0 || currentProcess->parent == 0)
+//     return -1;
+//   return currentProcess->parent->pid;
+// }
+
+// // Kernel function for returning grandparent PID
+// int
+// getgpid(void)
+// {
+//   struct proc* currentProcess = myproc();  // Fetch the current process structure.
+//   // Null check
+//   if(currentProcess == 0 || currentProcess->parent == 0 || currentProcess->parent->parent == 0)
+//     return -1;
+//   return currentProcess->parent->parent->pid;
+// }
+
+// System call for getppid()
 int
-getppid(void)
+sys_getppid(void)
 {
   struct proc *currentProcess = myproc();  // Fetch the current process structure.
   // Check if the current process or its parent is NULL.
@@ -18,29 +40,15 @@ getppid(void)
   return currentProcess->parent->pid;
 }
 
-// Kernel function for returning grandparent PID
+// System call for getgpid()
 int
-getgpid(void)
+sys_getgpid(void)
 {
   struct proc* currentProcess = myproc();  // Fetch the current process structure.
   // Null check
   if(currentProcess == 0 || currentProcess->parent == 0 || currentProcess->parent->parent == 0)
     return -1;
   return currentProcess->parent->parent->pid;
-}
-
-// System call for getppid()
-int
-sys_getppid(void)
-{
-  return getppid();
-}
-
-// System call for getgpid()
-int
-sys_getgpid(void)
-{
-  return getgpid();
 }
 
 
