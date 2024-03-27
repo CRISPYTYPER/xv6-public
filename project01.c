a5e3c992f2e0670ab80b22abdad42ea9874e3cd9
CRISPYTYPER <jack3409@naver.com>
2024-03-27 07:39:37 +0000

    modify project01.c to add getpid and getppid

--- project01.c
+++ project01.c
@@ -5,6 +5,8 @@
 int
 main(int argc, char *argv[])
 {
+  int pid = getpid();
   int ppid = getppid();
-  printf(1, "Parent PID : %d\n", ppid);
+  printf(1, "Parent PID : %d\n", pid);
+  printf(1, "Grand Parent PID : %d\n", ppid);
 }
