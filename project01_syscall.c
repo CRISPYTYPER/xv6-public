0d70cda28433b03457ed2958ec7914f58c36fca9
CRISPYTYPER <jack3409@naver.com>
2024-03-27 14:05:20 +0000

    add getgpid and sys_getgpid in project01_syscall.c

--- project01_syscall.c
+++ project01_syscall.c
@@ -7,7 +7,7 @@
 #include "x86.h"
 #include "syscall.h"
 
-// System call for returning parent PID
+// Kernel function for returning parent PID
 int
 getppid(void)
 {
@@ -15,16 +15,32 @@ getppid(void)
   // Check if the current process or its parent is NULL.
   if(currentProcess == 0 || currentProcess->parent == 0)
     return -1;
-  return myproc()->parent->pid;
+  return currentProcess->parent->pid;
 }
 
-// Wrapper for getppid()
+// Kernel function for returning grandparent PID
+int
+getgpid(void)
+{
+  struct proc* currentProcess = myproc();  // Fetch the current process structure.
+  // Null check
+  if(currentProcess == 0 || currentProcess->parent == 0 || currentProcess->parent->parent == 0)
+    return -1;
+  return currentProcess->parent->parent->pid;
+}
+
+// System call for getppid()
 int
 sys_getppid(void)
 {
-  int ppid;
-  ppid = getppid();
-  return ppid;
+  return getppid();
+}
+
+// System call for getgpid()
+int
+sys_getgpid(void)
+{
+  return getgpid();
 }
 
 
