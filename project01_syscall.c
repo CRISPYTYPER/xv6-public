129e5af26118abd4bcca5defff045bdb20242e6e
CRISPYTYPER <jack3409@naver.com>
2024-03-28 01:26:55 +0000

    deleted comments(kernel functions)

--- project01_syscall.c
+++ project01_syscall.c
@@ -7,28 +7,6 @@
 #include "x86.h"
 #include "syscall.h"
 
-// // Kernel function for returning parent PID
-// int
-// getppid(void)
-// {
-//   struct proc *currentProcess = myproc();  // Fetch the current process structure.
-//   // Check if the current process or its parent is NULL.
-//   if(currentProcess == 0 || currentProcess->parent == 0)
-//     return -1;
-//   return currentProcess->parent->pid;
-// }
-
-// // Kernel function for returning grandparent PID
-// int
-// getgpid(void)
-// {
-//   struct proc* currentProcess = myproc();  // Fetch the current process structure.
-//   // Null check
-//   if(currentProcess == 0 || currentProcess->parent == 0 || currentProcess->parent->parent == 0)
-//     return -1;
-//   return currentProcess->parent->parent->pid;
-// }
-
 // System call for getppid()
 int
 sys_getppid(void)
