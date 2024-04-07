e241b9e18dc71341fd507a0b123a921678e70312
CRISPYTYPER <jack3409@naver.com>
2024-04-07 11:46:02 +0000

    add frames of required system calls in proc.c

--- proc.c
+++ proc.c
@@ -532,3 +532,60 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+// Project 02
+// Required system calls
+void 
+yield(void) 
+{
+  // 자신이 점유한 cpu를 양보합니다  
+}
+
+int 
+getlev(void)
+{
+  /**
+   * 프로세스가 속한 큐의 레벨을 반환합니다.
+   * MoQ에 속한 프로세스인 경우 99를 반환합니다.
+  */
+}
+
+int
+setpriority(int pid, int priority)
+{
+  /**
+   * 특정 pid를 가지는 프로세스의 priority를 설정합니다.
+   * priority 설정에 성공한 경우 0을 반환합니다.
+   * 주어진 pid를 가진 프로세스가 존재하지 않는 경우 -1을 반환합니다.
+   * priority가 0 이상 10 이하의 정수가 아닌 경우 -2를 반환합니다.
+  */
+}
+
+int
+setmonopoly(int pid, int password)
+{
+  /**
+   * 특정 pid를 가진 프로세스를 MoQ로 이동합니다. 인자로 독점 자격을 증명할 암호(자신의 학번)을 받습니다.
+   * 암호가 일치할 경우, MoQ를 반환합니다.
+   * 존재하지 않는 포르세스의 pid인 경우 -1을 반환합니다.
+   * 암호가 일치하지 않는 경우 -2를 반환합니다.
+   * 이미 MoQ에 존재하는 프로세스인 경우 -3을 반환합니다.
+   * 자기 자신을 MoQ로 이동시키려 하는 경우 -4를 반환합니다.
+  */
+}
+
+void
+monopolize(void)
+{
+  /**
+   * MoQ의 프로세스가 CPU를 독점하여 사용하도록 설정합니다.
+  */
+}
+
+void
+unmonopolize(void)
+{
+  /**
+   * 독점적 스케줄링을 중지하고 기존의 MLFQ part로 돌아갑니다.
+  */
+}
