005e9804beb399e447f87e02f96204d91f391471
CRISPYTYPER <jack3409@naver.com>
2024-04-14 02:59:18 +0000

    implement sys_getlev()

--- sysproc.c
+++ sysproc.c
@@ -104,6 +104,9 @@ sys_print_ticks_pid_name(void) {
 // Required system calls
 int
 sys_yield(void) {
+  /**
+   * 자신이 점유한 cpu를 앙보합니다.
+  */
   yield();
   return 0;
 }
@@ -115,6 +118,15 @@ sys_getlev(void)
    * 프로세스가 속한 큐의 레벨을 반환합니다.
    * MoQ에 속한 프로세스인 경우 99를 반환합니다.
   */
+  if(myproc()->state == RUNNABLE){ // RUNNABLE 한 상태에 있는 프로세스에만 유효함
+    if(myproc()->ismoq == 0){  // 일반 mlfq에 들어있는 프로세스이면
+      return myproc()->qnum;
+    }else{  // 만약 moq에 들어있는 프로세스이면
+      return 99;
+    }
+  }else{
+    return -1;
+  }
 }
 
 int
